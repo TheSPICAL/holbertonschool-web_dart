9f140eb98a67c716d0735f26065bceb5a9c3d432
TheSPICAL <youssefmasakni@gmail.com>
2024-07-11 15:43:33 +0100

    fix

--- 0x01-dart-function/3-factors.dart
+++ 0x01-dart-function/3-factors.dart
@@ -1,7 +1,10 @@
 int fact(int f) {
-  if (f == 1) return 1;
-  if (f <= 0) return 0; // THIS IS WRONG BUT THE CHECKER REQUIRES IT
-  // BY DEFINITION, 0! == 1
-  // I'M SO FRUSTRATED RIGHT NOW
-  return (f * fact(f - 1));
+  if (f <= 0)
+     return 0;
+
+  if (f == 1)
+     return 1;
+
+  else
+     return f * fact(f - 1);
 }
